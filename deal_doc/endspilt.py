a6ce954259840f7efda602bbfa52fc6a08c87e79
Baby_Yang <327485245@qq.com>
2019-10-10 16:00:36 +0800

    --add some new methods--

--- deal_doc/endspilt.py
+++ deal_doc/endspilt.py
@@ -51,6 +51,27 @@ def __add_space__(file_in,file_out):
                 file_obj.write(line)
 
 
+def __pair2line__(file_in_1, file_in_2, file_out):
+    """
+    讲分开的对联合并在一起
+    :param file_in_1:
+    :param file_in_2:
+    :param file_out:
+    :return:
+    """
+    f1 = open(file_in_1, 'r', encoding='utf-8')
+    f2 = open(file_in_2, 'r', encoding='utf-8')
+    try:
+        for line1,line2 in zip(f1.readlines(),f2.readlines()):
+            with open(file_out,'a',encoding='utf-8') as f:
+                f.write(line1)
+                f.write(line2)
+    finally:
+        f1.close()
+        f2.close()
+        f.close()
+
+
 def __line2pair__(file_in,file_out_1,file_out_2):
     """
     将quchong后的上下联拆开到各自的文件中
@@ -91,23 +112,28 @@ def __check_zishu__(file_in_1,file_in_2,file_out_1,file_out_2):
                         file_obj.write(line2)
 
 
-def __binghang__(file_in,file_out):
+def __binghang__(file_in, file_out, symbol):
     """
     将上下联合并到一行的操作
-     :param file_in: "../data/others.txt"
+    :param file_in:  "../data/others.txt"
     :param file_out: "../data/re1.txt"
+    :param symbol: 拼起来的两句按照什么分隔符拼接
     :return:
     """
-    lines = open(file_in, encoding="UTF-8").read()
-    fp = open(file_out, "w", encoding="UTF-8")
-    liness = lines.split("\n")
-    print(liness)
-    # for i in range(0,len(liness)):
-    #        fp.write(liness[i].replace("\u3000\u3000",""))
-    for i in range(0, len(liness), 2):
-        print(liness[i] + " " + liness[i + 1])
-        fp.write(liness[i] + "\t" + liness[i + 1] + "\n")
-    fp.close()
+    try:
+        lines = open(file_in, encoding="UTF-8").read()
+        fp = open(file_out, "w", encoding="UTF-8")
+        liness = lines.split("\n")
+        #print(liness)
+        # for i in range(0,len(liness)):
+        #        fp.write(liness[i].replace("\u3000\u3000",""))
+        for i in range(0, len(liness), 2):
+            # print(liness[i] + " " + liness[i + 1])
+            fp.write(liness[i] + symbol + liness[i + 1] + "\n")
+    except IndexError:
+        print("Error:",IndexError)
+    finally:
+        fp.close()
 
 
 def is_number(s):
@@ -134,7 +160,7 @@ def is_number(s):
 
 def __spilt_byotherchar__(file_in,file_out):
     """
-    去掉文本中出文字以外的信息
+    去掉文本中出文字以外的信息(目前使用穷举)
     :param file_in: "../data/second_old.txt"
     :param file_out: "../data/second.txt"
     :return:
@@ -193,9 +219,68 @@ def __quchong__(file_in,file_out):
     f.close()
 
 
+def __castdown__(file_in, file_out, num):
+    """
+    截断文件的前num行
+    :param file_in:
+    :param file_out:
+    :param num: 截断的行数
+    :return:
+    """
+    with open(file_in,'r',encoding='utf-8') as f:
+        for ii, line in enumerate(f):
+            if ii == num:
+                break
+            with open(file_out,'a',encoding='utf-8') as f_obj:
+                f_obj.write(line)
+
+
+def __pingjie__(file_in, file_out):
+    """
+    将两个文件拼在一起
+    :param file_in:
+    :param file_out:
+    :return:
+    """
+    try:
+        with open(file_in,'r',encoding='utf-8') as f:
+            for line in f:
+                with open(file_out,'a',encoding='utf-8') as f_obj:
+                    f_obj.write(line)
+    finally:
+        f.close()
+        f_obj.close()
+
+
+def __test__(file_in_1, file_in_2, file_out):
+    """
+    把无情对和对联拼起来
+    :param file_in_1: 对联
+    :param file_in_2: 无情对
+    :param file_out:  结果文件
+    :return:
+    """
+    f1 = open(file_in_1, 'r', encoding='utf-8')
+    f2 = open(file_in_2, 'r', encoding='utf-8')
+    try:
+        for line in f1.readlines():
+            line.strip('\n')
+            with open(file_out, 'a', encoding='utf-8') as f:
+                f.write(line + "||" + "0" + "\n")
+        for line2 in f2.readlines():
+            line2.strip("\n")
+            with open(file_out, 'a', encoding='utf-8') as f:
+                f.write(line2 + "||" + "1" + "\n")
+    finally:
+        f1.close()
+        f2.close()
+        f.close()
+
+
 if __name__ == '__main__':
-    #__check_zishu__()
     print("start..")
-    file_in = "../data/out2.txt"
-    __delete_line__(file_in,file_out="../data/out3.txt")
+    # file_in = "../data/d_result.txt"
+    # file_out = "../data/a.txt"
+    # __binghang__(file_in,file_out,symbol="")
+
     print("end...")
